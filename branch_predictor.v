1ddd8a421d0b4aa5d70b496aab00cca2c37cf63c
jciupis <jedrzej.ciupis@gmail.com>
2018-11-06 23:17:53 +0100

    Implement execution logic and clean up the state machine module

--- branch_predictor.v
+++ branch_predictor.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 30.10.2018 12:40:03
-// Design Name: 
-// Module Name: branch_predictor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module branch_predictor(
     input clk,
@@ -30,61 +10,95 @@ module branch_predictor(
     output [31:0] f_predict_addr,    // (FETCH)  Address that is predicted.
     output f_predict_valid           // (FETCH)  Flag that indicates if the prediction made should be used.
     );
-    
+
+    // Helper variables.
+    reg [31:0] f_out_addr = 31'b0;   // Address of target branch predicted in FETCH stage.
+    reg f_out_valid = 1'b0;          // Flag that indicates if a prediction performed in FETCH stage is valid.
+    reg f_addr_found = 1'b0;         // Flag that indicates if a PC from FETCH stage appears in the in_addr_array table.
+    reg d_addr_found = 1'b0;         // Latched f_addr_found flag available in DECODE stage.
+    reg d_fsm_reset = 1'b0;          // Flag that indicates that a given state machine entry should be reset.
+    reg [7:0] d_fsm_index = 7'b0;    // Index of an entry in state machine to modify.
+    reg x_branch_processed = 1'b0;   // Flag that indicates that a branch instruction was processed and execution feedback is valid.
+    reg x_fsm_set = 1'b0;            // Flag that indicates that a given state machine entry should be updated.
+    reg [7:0] x_fsm_index = 7'b0;    // Index of an entry in state machine to update in EXEC stage.
+    integer entry_to_replace = 0;	 // Index of address table entry to replace.
+
+    // Predictor's variables.
     reg [31:0] in_addr_array [3:0];  // Table of stored PC values containing a branch instruction.
     reg [31:0] out_addr_array [3:0]; // Table of predicted target branches for the stored PC values.
-    reg [31:0] tmp_out_addr = 31'b0; // Helper address of a temporary target branch.
-    reg tmp_out_valid = 1'b0;        // Helper flag that indicates if a prediction is valid.
-    reg f_addr_found = 1'b0;         // Flag that indicates if a given PC appears in the in_addr_array table (FETCH)
-    reg d_addr_found = 1'b0;         // Flag that indicates if a given PC appears in the in_addr_array table (DECODE)
-	integer entry_to_replace = 0;	 // Index of address table entry to replace.
-    
-    
-    // (FETCH) Return a prediction if provided PC appears in the in_addr_array table.
+    two_bit_counter state_machine    // The branch predictor's 2bit state machine.
+    (
+        .clk(clk),
+        .feedback(x_predict_res),
+        .set(x_fsm_set),
+        .set_index(x_fsm_index),
+        .reset(d_fsm_reset),
+        .reset_index(d_fsm_index),
+        .prediction()
+    );
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // (FETCH) Return a prediction if provided PC appears in the in_addr_array table. //////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     integer index;
     always @(posedge clk)
     begin
         // Initialize temporary outputs.
-        tmp_out_addr <= 31'b0;
-        tmp_out_valid <= 1'b0;
+        f_out_addr <= 31'b0;
+        f_out_valid <= 1'b0;
         
         // Iterate over stored PC values to check for provided PC.
         for (index = 0; index < 3; index = index + 1)
         begin
             if (f_pc == in_addr_array[index])
             begin
-                tmp_out_addr = out_addr_array[index];
-                tmp_out_valid = 1'b1;
+                // TODO: Make a proper prediction utilizing the state machine.
+                f_out_addr = out_addr_array[index];
+                f_out_valid = 1'b1;
                 
                 // This variable is set to 0 after it's handled.
-                f_addr_found = 1'b1;  // If found, then we don't need to re-check it in DECODE stage
+                f_addr_found = 1'b1;
+                
+                // Latch index of the state machine entry to be updated when the EXEC stage feedback is provided.
+                x_fsm_index = index;
             end
         end
     end
-    
-    // (DECODE) Update in_addr_array if provided PC actually was a branch instruction.
-    //		 If the PC provided in FETCH was not a branch, this step should be ignored.
-    //       If the PC provided in FETCH was a branch and it appears in in_addr_array, this step should be ignored.
-    //       If the PC probided in FETCH was a branch and it doesn't appear in in_addr_array, the PC provided
-    //        in DECODE should be inserted in the in_addr_array and corresponding out_addr_array entry should be reset.
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // (DECODE) Update in_addr_array if provided PC actually was a branch instruction. /////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 	always @(posedge clk)
 	begin
+	    d_fsm_reset = 1'b0;
+	    
 		if (d_is_branch == 1'b1)
 		begin
+		    // EXEC stage feedback is valid.
+		    x_branch_processed = 1'b1;
+		    
 			// A new branch instruction discovered. Insert it into the address table.
-			// TODO: Potential race condition with line 59
 			if (d_addr_found == 1'b0)
 			begin
 				d_addr_found = 1'b0;
 				
+				// Wrap up after the end of the array.
 				if (entry_to_replace == 4)
 				begin
 					entry_to_replace = 0;
 				end
 				
+				// Initialize the predictor for the specified address.
 				in_addr_array[entry_to_replace] = d_pc;
+				// TODO: Actually decode the target address to put it here instead of a 0.
 				out_addr_array[entry_to_replace] = 31'b0;
+				d_fsm_index = entry_to_replace;
+				d_fsm_reset = 1'b1;
 				
+				// Latch index of the state machine entry to be updated when the EXEC stage feedback is provided.
+				x_fsm_index = entry_to_replace;
+				
+				// Select next entry to be replaced.
 				entry_to_replace = entry_to_replace + 1;
 				
 			end
@@ -95,10 +109,24 @@ module branch_predictor(
 		d_addr_found = f_addr_found;
 	end
     
-    // (EXEC) Update given branch's state machine according to result of the branch instruction.
-    // TODO: Two-bit state machine should be implemented and its state should be changed appropriately.
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // (EXEC) Update given branch's state machine according to result of the branch instruction.  //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    always @(posedge clk)
+    begin
+        x_fsm_set = 1'b0;
+
+        // Branch instruction was processed. Predictor's state machine must be updated.
+        if (x_branch_processed == 1'b1)
+        begin
+            x_branch_processed = 1'b0;
+
+            // Index of the entry to be updated is already set.
+            x_fsm_set = 1'b1;
+        end
+    end
     
-    assign f_predict_addr  = tmp_out_addr;
-    assign f_predict_valid = tmp_out_valid;
+    assign f_predict_addr  = f_out_addr;
+    assign f_predict_valid = f_out_valid;
 
 endmodule
--- two_bit_counter.v
+++ two_bit_counter.v
@@ -1,42 +1,49 @@
 `timescale 1ns / 1ps
 
-
-// 2-bit state machine, for each address in the BTB
 module two_bit_counter(
 	input clk,
-	input feedback,			// x_predict_res
-	input fb_valid,			// flag that the x_predict_res we are reading is valid
-	input [7:0] index,      // Index of which entry we are currently working on
-	output reg prediction	// taken - not-taken bit
+	input feedback,		      // Flag that indicates if an executed branch instruction was taken (1) or not taken (0).
+	input set,                // Flag that indicates that the entry pointed to by index should be updated.
+	input [7:0] set_index,    // Index of an entry to update.
+	input reset,              // Flag that indicates that the entry pointed to by index should be reset.
+	input [7:0] reset_index,  // Index of an entry to reset.
+	output prediction	      // Flag that indicates if the branch is predicted to be taken (1) or not taken (0).
 );
-reg [1:0] state[3:0];   // TODO: make this parameterizable
 
-always wait(fb_valid)
-	// Update the state with the feedback from EX stage that is now available
-begin
-	if (feedback == 1'b1)
-	begin
-	   case(state[index])
-	   2'b00: state[index] = 2'b01;
-	   2'b01: state[index] = 2'b10;
-	   2'b10: state[index] = 2'b11;
-	   endcase
-    end
-    else // ?
-    begin
-       case(state[index])
-       2'b01: state[index] = 2'b00;
-       2'b11: state[index] = 2'b10;
-       2'b10: state[index] = 2'b01;
-       endcase
-    end
-       
-end
+// TODO: make this parameterizable
+reg [1:0] state [3:0];        // Array of predictor's state machines; each entry corresponds to a single PC stored in the predictor.
 
+// Resetting process.
 always @(posedge clk)
 begin
-
-	assign prediction = state[1];
+    if (reset == 1'b1)
+    begin
+        // Initialize the state as weakly not taken.
+        state[reset_index] = 2'b01;
+    end
 end
+
+// Updating process.
+always @(posedge clk)
+begin
+    if (set == 1'b1)
+    begin
+        if (feedback == 1'b1)
+            case (state[set_index])
+                2'b00: state[set_index] = 2'b01;
+                2'b01: state[set_index] = 2'b10;
+                2'b10: state[set_index] = 2'b11;
+            endcase
+        else
+            case (state[set_index])
+                2'b01: state[set_index] = 2'b00;
+                2'b10: state[set_index] = 2'b01;
+                2'b11: state[set_index] = 2'b10;
+            endcase
+    end
+end
+
+// TODO: Allow for returning a proper prediction by adding more inputs (get + get_index ?). 
+assign prediction = 1'b0;
 
 endmodule
