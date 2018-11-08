c1b05ee338003b9f5d6165fafe5aa497f2459560
Orestis Korakitis <orestis.korakitis@bsc.es>
2018-11-08 18:38:06 +0100

    Cosmetic fix (removed Vivado template)

--- tb2_branch_predictor.v
+++ tb2_branch_predictor.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.11.2018 12:22:59
-// Design Name: 
-// Module Name: tb_branch_predictor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_branch_predictor(
 
